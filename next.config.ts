ab6fdc539cccfb4d79195dfcd7ab6ccf4697cb66
Saiikishen <saiikishen@gmail.com>
2025-06-08 08:38:23 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -16,8 +17,19 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'firebasestudio-hosting.web.app',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
+  experimental: {
+    allowedDevOrigins: [
+      "https://6000-firebase-studio-1748533474989.cluster-73qgvk7hjjadkrjeyexca5ivva.cloudworkstations.dev"
+    ],
+  }
 };
 
 export default nextConfig;
